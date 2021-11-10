f15ddf28e70b5ffecb558438037f6b3cc95e4ec4
David Potapkin <david.potapkin@dataart.com>
2021-11-10 16:05:52 +0200

    init commit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { useState,  useEffect } from 'react'
+// import zafClient from '../../zafClient'
+import { Grid, Row, Col } from '@zendeskgarden/react-grid'
+import axios from "axios"
+import {ORDERS_MOCK} from "../../mock"
 import './style.scss'
-import zafClient from '../../zafClient'
-import Footer from '../Footer'
 
 const App = () => {
-  const [requester, setRequester] = React.useState(null)
+  useEffect(async () => {
+    const response = await axios.get('https://api.bigcommerce.com/stores/be72yuzniz/v3/orders', {
+      headers: {
+        "X-Auth-Token": 'bmabnfgcbndbwsx6nzhe3v21ftdv25u',
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    })
 
-  React.useEffect(async () => {
-    zafClient.invoke('resize', { height: '75px' })
-
-    const data = await zafClient.get('ticket.requester')
-    const requester = data['ticket.requester']
-    setRequester(requester)
+    console.log('Response:', response)
   }, [])
 
   return (
-    <div className="App">
-      <p className="Requester-name">Requesters name is {requester ? requester.name : ''}.</p>
-      <Footer />
+    <div>
+      <div className="App">
+        <p className="Requester-name">Host name is David.</p>
+        <button className="button-center" onClick={() => setIsShow(!isShow)}>Toggle!</button>
+      </div>
+      <Grid debug>
+        { ORDERS_MOCK.map(item => (
+          <Row justifyContent="center">
+            <Col size={4}>
+              <h3>Cart Id:</h3>
+              <div>{item.cart_id}</div>
+            </Col>
+            <Col size={4}>
+              <h3><b>Billing Address</b></h3>
+              <div>Email: {item.billing_address.email} </div>
+              <div>Full name: {`${item.billing_address.first_name} ${item.billing_address.last_name}`}</div>
+              <div>City: {item.billing_address.city}</div>
+              { item.billing_address.phone && <div>Phone: {item.billing_address.phone}</div> }
+            </Col>
+          </Row>
+        ))}
+      </Grid>
     </div>
   )
 }
